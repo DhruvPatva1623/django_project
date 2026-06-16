92988abbeee95c205071dcb9b9df55d07dba70f9
DhruvPatva1623 <dhruvnpatva01@gmail.com>
2026-06-16 14:33:35 +0530

    1st Commit

---
+++ myuserapp/urls.py
@@ -0,0 +1,11 @@
+#urls.py is used to navigate through the app and project
+from django.urls import path
+from . import views
+
+urlpatterns = [ 
+    path('', views.homepageview),
+    path('home',views.homepageview),
+    path('about',views.aboutpageview),
+    path('contact',views.contactpageview),
+    path('hello',views.hello)
+]
